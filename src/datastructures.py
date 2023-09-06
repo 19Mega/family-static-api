97932a0b3f85b7fbb1937086571ff51dcbe84ed8
Matias Borba <85084252+19Mega@users.noreply.github.com>
2023-09-06 16:01:22 +0000

    datastructures.py class functions aded

--- src/datastructures.py
+++ src/datastructures.py
@@ -8,6 +8,20 @@ update this file to implement the following already declared methods:
 """
 from random import randint
 
+    # + id: Int
+    # + first_name: String
+    # + last_name: String (Siempre Jackson)
+    # + age: Int > 0
+    # + lucky_numbers: Array of int
+
+    # self._members = [{
+    #     "id": self._generateId(),
+    #     "first_name": "John",
+    #     "last_name": last_name,
+    #     "age": Int > 0
+    #     "lucky_numbers": Array of int
+    # }]
+
 class FamilyStructure:
     def __init__(self, last_name):
         self.last_name = last_name
@@ -21,15 +35,23 @@ class FamilyStructure:
 
     def add_member(self, member):
         # fill this method and update the return
-        pass
+        if not member['id']:
+            member['id'] = self._generateId()
 
+        self._members.append(member)
+        
     def delete_member(self, id):
         # fill this method and update the return
-        pass
-
+        for member in self._members:
+            if member['id'] == id:
+                self._members = list(filter(lambda member: member['id'] != id, self._members))
+                return {"done": True}
+            
     def get_member(self, id):
         # fill this method and update the return
-        pass
+        for member in self._members:
+            if member['id'] == id:
+                return member
 
     # this method is done, it returns a list with all the family members
     def get_all_members(self):
